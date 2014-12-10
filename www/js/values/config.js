f70c803d5a13b24561c95dfc729443eaf306081e
Melanie L. Fryman <melanie@frymanet.com>
2014-12-09 19:44:24 -0600

    edited config.js

--- www/js/values/config.js
+++ www/js/values/config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular.module('starter')
-  // can also run on local host http://localhost:8080
-  .value('origin', 'http://pacific-tundra-7784.herokuapp.com');
+  // can also run on local host http://localhost:8080 OR localtunnel
+  .value('origin', 'https://pacific-tundra-7784.herokuapp.com');
 })();
