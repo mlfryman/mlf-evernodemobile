c2bb7cc8e884fb43a5be7233b69d36b1b52ba410
Melanie L. Fryman <melanie@frymanet.com>
2014-12-11 09:51:31 -0600

    rebuilt ionic

--- www/js/values/config.js
+++ www/js/values/config.js
@@ -3,5 +3,5 @@
 
   angular.module('starter')
   // can also run on localhost http://localhost:8080, heroku https://evernode.herokuapp.com, OR localtunnel
-  .value('origin', 'https://cucqdfppwy.localtunnel.me');
+  .value('origin', 'https://ugjwxmznnn.localtunnel.me');
 })();
